d00574382858525f07f8b4c7dae00cb298947e47
Alessandro Calista <alessandromaria.calista@gmail.com>
2026-01-20 12:41:44 +0100

    Rewritten knockback calculation

--- src/main/java/it/alessandrocalista/knockbackstick/listener/HitListener.java
+++ src/main/java/it/alessandrocalista/knockbackstick/listener/HitListener.java
@@ -1,21 +1,27 @@
 package it.alessandrocalista.knockbackstick.listener;
 
 import it.alessandrocalista.knockbackstick.KnockbackStick;
-import it.alessandrocalista.knockbackstick.util.KnockbackUtil;
+import it.alessandrocalista.knockbackstick.util.VelocityModifier;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Vector;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class HitListener implements Listener {
     private static final double DEFAULT_MULTIPLIER = 1d;
+    private final Map<UUID, VelocityModifier> knockedEntities = new HashMap<>();
 
     @EventHandler
     public void onHit(EntityDamageByEntityEvent entityDamageByEntityEvent) {
@@ -38,8 +44,28 @@ public class HitListener implements Listener {
                 DEFAULT_MULTIPLIER
         );
 
-        Vector direction = damager.getLocation().getDirection();
-        KnockbackUtil.applySegmentedKnockback(victim, direction, strength);
+        knockedEntities.put(victim.getUniqueId(), new VelocityModifier(damager.getLocation().getDirection(), strength));
+    }
+
+    @EventHandler
+    public void onKnockback(PlayerVelocityEvent velocityEvent) {
+        Player victim = velocityEvent.getPlayer();
+        UUID victimId = victim.getUniqueId();
+        if(!knockedEntities.containsKey(victimId)) return;
+
+        VelocityModifier velocityModifier = knockedEntities.get(victimId);
+        double strength = velocityModifier.strength();
+        Vector attackersDirection = velocityModifier.attackersVector();
+
+        Vector direction = attackersDirection.clone().setY(0).normalize();
+
+        direction.multiply(strength);
+
+        double vLift = Math.log10(strength + 1);
+        direction.setY(vLift);
+
+        velocityEvent.setVelocity(direction);
+        knockedEntities.remove(victimId);
     }
 
 }
